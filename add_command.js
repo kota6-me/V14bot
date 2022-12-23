586c813f2d79199543be772ab22aef9f391c3472
Ritsu.me <mail@ritsu-me.com>
2022-12-24 03:13:10 +0900

    V14対応

--- add_command.js
+++ add_command.js
@@ -1,69 +1,40 @@
-const config = require("./config.js");
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v10');
-const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN)
-const fs = require("fs");
+const { REST, Routes } = require('discord.js');
+const config = require('./config.json');
+const fs = require('node:fs');
 
-const clientId = config.clientId;
-const testGuild = config.dev.testGuild;
+const commands = [];
+const adminCommands = [];
+const commandFiles = fs.readdirSync('./commands/general').filter(file => file.endsWith('.js'));
+const adminCommandFiles = fs.readdirSync("./commands/admin").filter(file => file.endsWith(".js"));
 
-let command_int = 0;
-const globalCommands = [];
-const adminGuildCommands = [];
-const commandFolders = fs.readdirSync("./commands");
-
-function cmdToArray(array, command, file, notice = "") {
-    try {
-        array.push(command.data.toJSON());
-        command_int++;
-        console.log(`${notice} ${file} が追加されました。`);
-    } catch (error) {
-        console.log(`\u001b[31m${notice} ${file} はエラーにより追加されませんでした。\nエラー内容\n ${error}\u001b[0m`);
-    }
+for (const file of commandFiles) {
+    const command = require(`./commands/general/${file}`);
+    commands.push(command.data.toJSON());
 }
 
-async function main(){
-    await rest.put(
-        Routes.applicationCommands(config.clientID),
-        { body: commands }
-    );
-    await rest.put(
-        Routes.applicationGuildCommands(config.clientID, config.dev.testGuild),
-        { body: adminCmd }
-    )
+for (const file of adminCommandFiles) {
+    const command = require(`./commands/admin/${file}`);
+    commands.push(command.data.toJSON());
 }
 
-for (const folder of commandFolders) {
-    console.log(`\u001b[32m===${folder} commands===\u001b[0m`);
-    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith(".js"));
-    for (const file of commandFiles) {
-        const command = require(`./commands/${folder}/${file}`);
-        if (command.adminGuildOnly) {
-            cmdToArray(adminGuildCommands, command, file, "[Admin]");
-            continue;
-        }
-        if (command.onlyCommand) continue;
-        cmdToArray(globalCommands, command, file, "[Global]");
-    }
-    console.log(`\u001b[32m===${folder} added===\u001b[0m`);
-}
-
-
-main().catch(err => console.log(err))
+const rest = new REST({ version: '10' }).setToken(config.token);
 
 (async () => {
     try {
-        console.log(`${command_int}個のスラッシュコマンドを登録/再登録します…`);
+        console.log(`${adminCommands.length}個のコマンドを読み込み中`);
+        const adminData = await rest.put(
+            Routes.applicationGuildCommands(config.clientID, config.dev.testGuild),
+            { body: adminCommands },
+        );
+        console.log(`[GuildCommand]${adminData.length}個のコマンドを読み込み完了`);
+        
+        console.log(`${commands.length}個のコマンドを読み込み中`);
+        const data = await rest.put(
+            Routes.applicationGuildCommands(config.clientID),
+            { body: commands },
+        );
+        console.log(`[GuildCommand]${data.length}個のコマンドを読み込み完了`);
 
-        //Admin
-        putToDiscord(adminGuildCommands, testGuild);
-        console.log("管理コマンドを正常に登録しました。");
-
-        //Global
-        putToDiscord(globalCommands);
-        console.log("グローバルコマンドを正常に登録しました。");
-
-        console.log("全てのスラッシュコマンドを正常に登録しました！");
     } catch (error) {
         console.error(error);
     }
