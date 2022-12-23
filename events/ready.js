9dc6f7dad024fac1f9085a575c5d9bfbf2eaf0e7
Ritsu.me <mail@ritsu-me.com>
2022-12-24 05:05:05 +0900

    ステータスメッセージ変更

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,21 @@
 const config = require("../config.js");
 const functions = require("../functions.js");
 
+const { EmbedBuilder } = require('discord.js');
+
 module.exports = {
     name: "ready", // イベント名
     async execute(client) {
-        client.user.setPresence({ activities: [{ name: `Type /help | Servers: ${client.guilds.cache.size}` }], status: "online" });
-        client.channels.cache.get(config.logch.ready).send("Discordログインしました！");
+        client.user.setPresence({ activities: [{ name: `My eyes are always on you👀` }], status: "online" });
+        client.channels.cache.get(config.logch.ready).send({
+            embeds: [
+                new EmbedBuilder()
+                .setTitle("起動完了")
+                .setDescription("> Botが起動しました。\n> 運営担当者は動作チェックをお願いします。")
+                .setColor("#2f3136")
+                .setTimestamp()
+            ]
+        });
         console.log(`[${functions.timeToJST(Date.now(), true)}] Logged in as ${client.user.tag}!`);
     }
 }
